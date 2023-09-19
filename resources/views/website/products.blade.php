90fb29542e7c300b35c9c97534706f50be89ba04
Jawad121 <jawad.ahmed149@gmail.com>
2023-09-19 17:33:32 +0500

    git ignor modified

--- resources/views/website/products.blade.php
+++ resources/views/website/products.blade.php
@@ -4,7 +4,7 @@
 <div class="container d-flex flex-wrap bg-light p-2 ">
     @foreach ($products as $product)
         <div class="col-3 d-flex justify-items-center flex-column me-2 mb-2 border rounded p-2">
-            <img src="{{vite('resources/images/p_1.png')}}" alt="" class="rounded" style="width: 100%; height: 200px !important"/>
+            <img src="{{Vite::asset('resources/images/p_1.png')}}" alt="" class="rounded" style="width: 100%; height: 200px !important"/>
             <h5 class="mt-2">
                 <span class="text-primary">Product Name : </span> {{$product->name}}
             </h5>
