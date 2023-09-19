59b1740c629daa80dca8ccc7dcdda0a19d6ee4c9
zrt1992 <zulfiqar.tariq@hotmail.com>
2023-09-19 17:18:52 +0500

    added mails

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
+    function index(Request $request)
+    {
+        $users = User::get();
+        return view('admin.index', $user);
+    }
+}
